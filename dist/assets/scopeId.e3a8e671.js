9391711925da9a2ae2e17752be2995d77c50892d
Maima Ismail <maimamirza@gmail.com>
2023-08-27 15:50:17 +0500

    dist commit

---
+++ dist/assets/scopeId.e3a8e671.js
@@ -0,0 +1,1 @@
+import{a8 as o}from"./index.ef6063b0.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
---
+++ dist/assets/useAppAbility.19f68757.js
@@ -0,0 +1,1 @@
+import{aM as i}from"./index.ef6063b0.js";const t=()=>i();export{t as u};
